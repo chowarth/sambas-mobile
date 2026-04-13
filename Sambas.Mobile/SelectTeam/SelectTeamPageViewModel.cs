184b9708a94256d614b1109b77ca51b21f0169ef
Chris Howarth <chowarth@users.noreply.github.com>
2026-04-13 11:13:36 +0100

    Refactor team selection page

--- Sambas.Mobile/SelectTeam/SelectTeamPageViewModel.cs
+++ Sambas.Mobile/SelectTeam/SelectTeamPageViewModel.cs
@@ -6,23 +6,17 @@ internal sealed record Team(Guid Id, string Name, string LogoUrl);
 
 internal sealed class SelectTeamPageViewModel : BaseViewModel
 {
-    private IList<Team> _teams = new List<Team>();
+    private IList<Team> _teams = [
+        new Team(
+            Guid.NewGuid(),
+            "Sambas",
+            "https://www.foresttownrangers.co.uk/wp-content/uploads/Forest-Town-Rangers-FC-Logo.png"
+        )
+    ];
+
     public IList<Team> Teams
     {
         get => _teams;
         set => SetProperty(ref _teams, value);
-    }
-
-    public override void OnAppearing()
-    {
-        base.OnAppearing();
-
-        Teams.Add(
-            new Team(
-                Guid.NewGuid(),
-                "Sambas",
-                "https://www.foresttownrangers.co.uk/wp-content/uploads/Forest-Town-Rangers-FC-Logo.png"
-            )
-        );
     }
 }

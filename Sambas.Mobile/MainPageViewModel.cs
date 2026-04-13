40234c14c747bf26c2bc47193719dfc28d4c982b
Chris Howarth <chowarth@users.noreply.github.com>
2026-04-13 12:06:39 +0100

    Remove unused page

--- Sambas.Mobile/MainPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-using Sambas.Mobile.Mvvm;
-
-namespace Sambas.Mobile;
-
-internal sealed class MainPageViewModel(ILogger<MainPageViewModel> logger)
-    : BaseViewModel(logger)
-{
-}
